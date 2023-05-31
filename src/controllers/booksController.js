2087189bd1b6f1610374a933e94d667bcb317c34
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-05-31 18:22:54 -0500

    Book Controllers Created

---
+++ src/controllers/booksController.js
@@ -0,0 +1,47 @@
+const { Book } = require('../models');
+const { bookSchema } = require('../schema');
+
+const createBook = async (req, res) => {
+  const validate = bookSchema.validate(req.body);
+  if (validate.error) {
+    return res.status(400).send(validate.error);
+  }
+  const book = await new Book(req.body).save();
+
+  res.json(book);
+};
+
+const getBookById = async (req, res) => {
+  const { id } = req.params;
+  const bookById = await Book.findById(id);
+  res.json(bookById);
+};
+
+const updateBook = async (req, res) => {
+  const { id } = req.params;
+  const validate = bookSchema.validate(req.body);
+  if (validate.error) {
+    return res.status(400).send(validate.error);
+  }
+  const book = await Book.findByIdAndUpdate(id, req.body);
+  res.json(book);
+};
+
+const getAllBooks = async (_, res) => {
+  const book = await Book.find();
+  res.json(book);
+};
+
+const deleteBook = async (req, res) => {
+  const { id } = req.params;
+  const book = await Book.findOneAndRemove(id, { new: true });
+  res.json({ message: `Book ${book.title} has been deleted` });
+};
+
+module.exports = {
+  createBook,
+  getAllBooks,
+  getBookById,
+  updateBook,
+  deleteBook,
+};
