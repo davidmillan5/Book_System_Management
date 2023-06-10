b54530b7c04360e208b66c3282196b7227b77668
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-06-10 11:36:33 -0500

    health check

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,8 @@ const router = Router();
 
 const { healthCheck, welcomePage } = require('../controllers/appController');
 
-router.get('/', welcomePage).get('/health', healthCheck);
+router.get('/', welcomePage);
+router.get('/health', healthCheck);
 router.use('/api/v1/books', require('./bookRoute'));
 router.use('/api/v1/users', require('./userRoute'));
 router.use('/api/v1/borrow', require('./borrowRoute'));
