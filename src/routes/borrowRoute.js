ecc6a3f307fbb7491a71bacea731966c7d806eac
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-06-12 23:21:11 -0500

    Borrow and Returned Method Authentication Up and Running

--- src/routes/borrowRoute.js
+++ src/routes/borrowRoute.js
@@ -1,11 +1,13 @@
 const express = require('express'),
   router = express.Router(),
-  { Borrow } = require('../controllers');
+  { Borrow } = require('../controllers'),
+  { verifyToken } = require('../middleware/authmiddleware');
 
 router.get('/health', (_, res) => {
   res.send('check');
 });
 
-router.route('/:isbn').post(Borrow.createBorrowedRecord);
+// router.route('/:isbn').post(Borrow.createBorrowedRecord);
+router.post('/:isbn', verifyToken, Borrow.createBorrowedRecord);
 
 module.exports = router;
--- src/routes/returnedRouted.js
+++ src/routes/returnedRouted.js
@@ -1,11 +1,13 @@
 const express = require('express'),
   router = express.Router(),
-  { Returned } = require('../controllers');
+  { Returned } = require('../controllers'),
+  { verifyToken } = require('../middleware/authmiddleware');
 
 router.get('/health', (_, res) => {
   res.send('check');
 });
 
-router.route('/:id').post(Returned.createReturnedRecord);
+// router.route('/:id').post(Returned.createReturnedRecord);
+router.post('/:id', verifyToken, Returned.createReturnedRecord);
 
 module.exports = router;
